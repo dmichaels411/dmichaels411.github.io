b6fa1aa9b60adeadb595934f57c085062ef0e92b
David Michaels <dmichaels411@gmail.com>
2024-01-28 15:14:51 -0500

    added pages and navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app/Routes.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './app/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <RouterProvider router={router} />
+  </React.StrictMode>
 )
